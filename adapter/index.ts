7f82a7db5fa0beb8e052db89824e5e5e92b78f4b
kwu-mce <wuk87@mcmaster.ca>
2024-05-10 21:17:04 +0000

    init, add dependencies, running koa server

--- adapter/index.ts
+++ adapter/index.ts
@@ -1,3 +1,12 @@
 import assignment from "./assignment-1";
 
 export default assignment;
+
+const koa = require('koa');
+const app = new koa();
+
+app.use((t: { body: string; }) => {
+    t.body = 'Helloe world!!';
+});
+
+app.listen(1231);
