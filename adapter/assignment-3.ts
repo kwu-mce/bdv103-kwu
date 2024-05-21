c2a798ec369f99e4adaff4c47ea618e7c58a9bb1
KSWU <wukev89@gmail.com>
2024-05-21 01:15:09 +0000

    Modify listBooks to call filteredbooks route with query params

--- adapter/assignment-3.ts
+++ adapter/assignment-3.ts
@@ -21,7 +21,47 @@ export interface Filter {
 // If multiple filters are provided, any book that matches at least one of them should be returned
 // Within a single filter, a book would need to match all the given conditions
 async function listBooks (filters?: Filter[]): Promise<Book[]> {
-  throw new Error("Todo")
+  try {
+    
+    let queryParams = '';
+
+    if (filters) {
+      // for (const filter of filters) {
+       
+      //     let filter_from = filter.from;
+      //     let filter_to = filter.to;
+      //     let filter_name = filter.name;
+      //     let filter_author = filter.author;
+
+      //     console.log(`filterfrom= ${filter_from}`);
+      //     console.log(`filterto= ${filter_to}`);
+      //     console.log(`filtername= ${filter_name}`);
+      //     console.log(`filterauthor= ${filter_author}`);
+      //   }
+      
+      filters.forEach((filter, index) => {
+
+        for (const key in filter) {
+          queryParams += `${key}=${filter[key as keyof Filter]}&`;
+        }
+        
+      });
+    }
+
+    if (queryParams.endsWith('&')) {
+      queryParams = queryParams.slice(0, -1);
+    }
+
+    console.log(`queryParams= ${queryParams}`);
+
+    const response = await fetch(`http://localhost:9000/filteredbooks?${queryParams}`);
+    const books: Book[] = await response.json() as Book[];
+
+    return books;
+
+  } catch {
+      throw new Error("Error finding book list")
+  }
 }
 
 async function createOrUpdateBook (book: Book): Promise<BookID> {
