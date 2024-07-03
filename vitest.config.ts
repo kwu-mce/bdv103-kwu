0cd7c85022ade8a52df9ed437f9debbb287dd60e
KSWU <wukev89@gmail.com>
2024-07-03 21:02:44 +0000

    generate client sdk

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vitest/config';
+import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
     test: {
@@ -7,6 +8,12 @@ export default defineConfig({
         globals: true,
         include: ['test/**/*.test.ts'],
         includeSource: ['src/**/*.{js,ts}'],
-        setupFiles: ['./test/setupMongoTest.ts']
+        setupFiles: ['./test/setupMongoTest.ts'],
+        exclude: ['**/node_modules/**', '**/dist/**', '**/cypress/**', '**/.{idea,git,cache,output,temp}/**', '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*'],
     },
+    plugins: [
+        legacy({
+            targets: ['defaults', 'not IE 11'],
+        }),
+    ],
 });
