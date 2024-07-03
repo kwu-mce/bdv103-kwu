7b39ab63e07d4009f4bbfcd0b402f1d4a961538b
KSWU <wukev89@gmail.com>
2024-07-03 18:43:25 +0000

    add tsoa and placeholder spec

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import Koa from 'koa';
 import router from './routes/bookRoutes';
+import { koaSwagger } from 'koa2-swagger-ui';
+import { Swagger } from 'tsoa';
 
 const cors = require('@koa/cors');
 const app = new Koa();
@@ -9,3 +11,14 @@ app.use(router.middleware());
 app.listen(9000, () => {
     console.log('app listening on http://localhost:9000');
 });
+
+app.use(koaSwagger({
+    routePrefix: '/docs',
+    specPrefix: '/docs/spec',
+    exposeSpec: true,
+    swaggerOptions: {
+      spec: Swagger 
+    }
+  
+  }))
+  
