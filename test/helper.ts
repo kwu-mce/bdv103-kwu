464cdadce90fe4f8f93d71101e86c9cb0e46ea97
KSWU <wukev89@gmail.com>
2024-07-04 03:19:20 +0000

    try to add api tests

---
+++ test/helper.ts
@@ -0,0 +1,22 @@
+import { beforeEach, afterEach } from "node:test";
+
+export function testSetup() {
+
+    beforeEach(() => {
+
+        //connect to db
+        //specify test book schema
+        //return connection? 
+    
+    });
+};
+
+
+export function testTerminate() {
+    afterEach(() => {
+
+        //remove db objects
+        //terminate connect
+        
+    });
+}
